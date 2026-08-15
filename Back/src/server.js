b12e44071f796ae50b20be11eb90b01e6916014e
Dantedevil10 <jonhjooj2907@gmail.com>
2026-08-15 09:52:43 -0300

    log change

--- Back/src/server.js
+++ Back/src/server.js
@@ -252,6 +252,6 @@ app.get('/api/transcricoes/:id/planilha', async (req, res) => {
     }
 });
 
-app.listen(PORT, () => console.log('🚀 API RESTful rodando na porta 3000')); //<- roda na nuvem
+app.listen(PORT, () => console.log('🚀 API RESTful rodando na porta'+PORT)); //<- roda na nuvem
 
 //app.listen(3000, () => console.log('🚀 API RESTful rodando na porta 3000')); <- roda Localmente
